c947c26db2ff84ab04a2d3e5d2f32cc4efd1d888
kazuhiro ogawa <ogawa.polytech@gmail.com>
2023-06-20 20:42:23 +0900

    SharedDataのメンバ変数をIterable<EmployeeInfo>型に変更

--- src/main/java/com/plma/model/shareddata/SharedData.java
+++ src/main/java/com/plma/model/shareddata/SharedData.java
@@ -1,39 +1,18 @@
 package com.plma.model.shareddata;
 
-import java.sql.Date;
-
 import org.springframework.stereotype.Component;
 
 import com.plma.model.entity.EmployeeInfo;
 
 @Component
 public class SharedData {
-    private EmployeeInfo ei;
+    private Iterable<EmployeeInfo> ei;
 
-    public void setEmployeeInfoOneByOne(String code,
-			Date join_date,
-			String hurigana_lastname,
-			String hurigana_firstname,
-			String lastname,
-			String firstname,
-			Integer department_number,
-			Integer working_days)
-    {
-        this.ei.setCode(code);
-        this.ei.setJoin_date(join_date);
-        this.ei.setHurigana_lastname(hurigana_lastname);
-        this.ei.setHurigana_firstname(hurigana_firstname);
-        this.ei.setLastname(lastname);
-        this.ei.setFirstname(firstname);
-        this.ei.setDepartment_number(department_number);
-        this.ei.setWorking_days(working_days);
-    }
-
-    public void setEmployeeInfo(EmployeeInfo ei) {
+    public void setEmployeeInfo(Iterable<EmployeeInfo> ei) {
     	this.ei = ei;
     }
 
-    public EmployeeInfo getEmployeeInfo() {
+    public Iterable<EmployeeInfo> getEmployeeInfo() {
         return this.ei;
     }
 }
