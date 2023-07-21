d1212009026ac7102a1592f4dce3a0551b9be1dc
mari495 <somari3915@gmail.com>
2023-07-21 22:43:08 +0900

    ManagementBookCreationControllerConboBox実装

--- src/main/java/com/plma/controller/ManagementBookCreationController.java
+++ src/main/java/com/plma/controller/ManagementBookCreationController.java
@@ -1,5 +1,12 @@
+/**このクラスは有給休暇管理簿を作成するためのクラスです
+ * PaidLeaveDto3というクラスを表示用に作成しました。
+ * 
+ * 
+ * 
+ */
 package com.plma.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ResourceBundle;
@@ -8,14 +15,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.plma.SpringFXMLLoader;
+import com.plma.model.entity.EmployeeInfo;
 import com.plma.model.entity.PaidLeaveDto3;
 import com.plma.model.service.EmployeeInfoService;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 @Controller
 public class ManagementBookCreationController {
@@ -93,13 +108,13 @@ public class ManagementBookCreationController {
     @FXML
     private TableColumn<PaidLeaveDto3, String> PaidLeave_date_col;
 
-
     @FXML
     private TableView<PaidLeaveDto3> datatable;
 
     
-
-
+    
+    @FXML
+    private ComboBox<String> name_ComboBox;
 
 
     @FXML
@@ -125,10 +140,15 @@ public class ManagementBookCreationController {
         assert id_col != null : "fx:id=\"id_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
         assert joindate_col != null : "fx:id=\"joindate_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
         assert lastname_col != null : "fx:id=\"lastname_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
-        assert menu_button != null : "fx:id=\"menu_button\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
+        
         assert reference_date_col != null : "fx:id=\"reference_date_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
         assert remaining_paid_leave_days_col != null : "fx:id=\"remaining_paid_leave_days_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
         assert working_days_col != null : "fx:id=\"working_days_col\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
+        
+        assert menu_button != null : "fx:id=\"menu_button\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
+        assert name_ComboBox != null : "fx:id=\"name_ComboBox\" was not injected: check your FXML file 'ManagementBookCreation.fxml'.";
+        
+        fiscal_year_col.setCellValueFactory(new PropertyValueFactory<PaidLeaveDto3, Date>("fiscal_year"));
         
         fiscal_year_col.setCellValueFactory(new PropertyValueFactory<PaidLeaveDto3, Date>("fiscal_year"));
         id_col.setCellValueFactory(new PropertyValueFactory<PaidLeaveDto3, Integer>("id"));
@@ -153,6 +173,37 @@ public class ManagementBookCreationController {
 		remaining_paid_leave_days_col.setCellValueFactory(new PropertyValueFactory<PaidLeaveDto3, Integer>("remaining_paid_leave_days"));
 		PaidLeave_date_col.setCellValueFactory(new PropertyValueFactory<PaidLeaveDto3, String>("PaidLeave_date"));
 		
+		//ComboBoxの初期化処理
 
+		//ComboBoxから値を取り出して変数へ保存
+		//DBから
+		Iterable<EmployeeInfo> result = service.selectAll();;//全データ抽出各columnに入れていく
+		System.out.println("result"+result);
+		// ComboBoxに値を追加
+	    for (EmployeeInfo employeeInfo : result) {
+	        String fullName = employeeInfo.getFirstname()  + employeeInfo.getLastname();
+	        name_ComboBox.getItems().add(fullName);
+	    }
     }
+    @FXML
+	void Menu_button_onClick(ActionEvent event) {
+
+		Scene s = ((Node)event.getSource()).getScene();
+		Window window = s.getWindow();
+		window.hide();
+
+		/*
+		 * 新しい画面を生成する
+		 */
+		try {
+			Parent parent = fxmlLoader.load(getClass().getResource("/com/plma/view/MainScene.fxml"));
+			Scene scene = new Scene(parent);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.setTitle("メインメニュー");
+			stage.show();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
